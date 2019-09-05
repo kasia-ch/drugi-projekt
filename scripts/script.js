d68df3909e1577beb239999831c2f5d2c76be810
My Love <checinska.k@gmail.com>
2019-09-05 21:23:01 +0200

    all // deleted

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,9 @@
-// Tutaj dodacie zmienne globalne do przechowywania elementów takich jak np. lista czy input do wpisywania nowego todo
 let $list, $modal, $buttonCancel, $buttonOk, $form, $myInput, lastId = 0, $popupInput, $editButton, currentId, $doneButton, $closePopup;
 const initialList = ['Dzisiaj robię usuwanie', 'Nakarm psa'];
 
 function main() {
   prepareDOMElements();
   prepareDOMEvents();
-  //prepareInitialList();
   getListFromServer();
 }
 
@@ -19,10 +17,9 @@ function getListFromServer () {
         });
       }
     });
-  }
+}
 
 function prepareDOMElements() {
-  // To będzie idealne miejsce do pobrania naszych elementów z drzewa DOM i zapisanie ich w zmiennych
   $list = document.getElementById('list');
   $modal = document.querySelector('#modal');
   $buttonCancel = document.querySelector('#btn__cancel');
@@ -49,12 +46,12 @@ function prepareDOMEvents() {
 
    $buttonOk.addEventListener('click', function () {
     acceptChangeHandler();
-  });
+});
+}
  
   $form.addEventListener('submit', function(e) {
     e.preventDefault(); {
       if ($myInput.value.trim() !=='') {
-        //addNewTodoToList();
         sendTodoToServer();
       }
     } 
@@ -63,28 +60,23 @@ function prepareDOMEvents() {
 
 
 function prepareInitialList() {
-  // Tutaj utworzymy sobie początkowe todosy. Mogą pochodzić np. z tablicy
   initialList.forEach(todo => {
     addNewElementToList(todo);
   });
 }
 
 function addNewElementToList(title, id) {
-  //obsługa dodawanie elementów do listy
-  // $list.appendChild(createElement('nowy', 2))
   const newElement = createElement(title, id);
   $list.appendChild(newElement);
-  
 }
 
 function createElement(title, id) {
   let div1 = document.createElement('div');
 
   const newElement = document.createElement('li');
-  //lastId += 1;
   newElement.setAttribute('data-id', id);
   let div2 = document.createElement('div');
-  const titleElement = document.createElement('span');//w jednym divie buttony
+  const titleElement = document.createElement('span');
   titleElement.innerText = title;
 
 
@@ -104,10 +96,8 @@ function createElement(title, id) {
   newElement.appendChild(editButton);
   newElement.appendChild(doneButton);
   newElement.appendChild(delButton);
-
   
   return newElement;
-
 }
 
 function addNewTodoToList() {
@@ -115,10 +105,7 @@ function addNewTodoToList() {
   $myInput.value = '';
 }
 
-
 function listClickManager(event) {
-  // Rozstrzygnięcie co dokładnie zostało kliknięte i wywołanie odpowiedniej funkcji
-  // event.target.parentElement.id
   let id = event.target.parentElement.dataset.id;
  
   if (event.target.className === 'btn-delete') { 
@@ -130,7 +117,6 @@ function listClickManager(event) {
     let title = document.querySelector('li[data-id="' + id + '"]').querySelector('span').innerText;
     editListElement(id, title);
   
-    
   } else if (event.target.className === 'btn-done') {
     event.target.parentElement.classList.toggle('done');
  }
@@ -138,29 +124,16 @@ function listClickManager(event) {
 }
 
 function removeListElement(id) {
-  //let liElement = document.querySelector('li[data-id="' + id + '"]');
- 
   axios.delete('http://195.181.210.249:3000/todo/' + id).then((response) => {
     if (response.data.status === 0) {
       getListFromServer();
     }
   });
- 
-  //$list.removeChild(liElement);
 }
 
 function editListElement(id, title) {
-  // Pobranie informacji na temat zadania
-  // Umieść dane w popupie
   openPopup();
   $popupInput.value = title;
-
-  //let liElement = document.querySelector('#' + id);
- // $list.editListElement(liElement);
-}
-
-function addDataToPopup(/* Title, author, id */) {
-  // umieść informacje w odpowiednim miejscu w popupie
 }
 
 function acceptChangeHandler() {
@@ -181,23 +154,11 @@ axios.post('http://195.181.210.249:3000/todo/', {title: $myInput.value}).then((r
 }
 
 function openPopup() {
-  // Otwórz popup
-
-    $modal.classList.add('modal--show');  
-
+  $modal.classList.add('modal--show');  
 }
 
 function closePopup() {
-  //let id = event.target.parentElement.id;
   $modal.classList.remove('modal--show');
-}
-
-function declineChanges() { //niepotrzebna raczej
-  // closePopup()
-}
-
-function markElementAsDone(/* id */) {
-  //zaznacz element jako wykonany (podmień klasę CSS)
 }
 
 document.addEventListener('DOMContentLoaded', main);
